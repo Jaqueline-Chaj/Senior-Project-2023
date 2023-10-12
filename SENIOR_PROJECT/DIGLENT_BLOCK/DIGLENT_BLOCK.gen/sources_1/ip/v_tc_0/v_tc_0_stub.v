5c8cfb4e66f3d3f6bd4b8083684975e41580822c
Evan Leopold <leopole1@lions.tcnj.edu>
2023-10-12 14:18:27 -0400

    commit

---
+++ SENIOR_PROJECT/DIGLENT_BLOCK/DIGLENT_BLOCK.gen/sources_1/ip/v_tc_0/v_tc_0_stub.v
@@ -0,0 +1,31 @@
+// Copyright 1986-2021 Xilinx, Inc. All Rights Reserved.
+// --------------------------------------------------------------------------------
+// Tool Version: Vivado v.2021.2 (win64) Build 3367213 Tue Oct 19 02:48:09 MDT 2021
+// Date        : Mon Oct  9 16:59:26 2023
+// Host        : ARM144-11 running 64-bit major release  (build 9200)
+// Command     : write_verilog -force -mode synth_stub
+//               h:/Senior-Project-2023/SENIOR_PROJECT/DIGLENT_BLOCK/DIGLENT_BLOCK.gen/sources_1/ip/v_tc_0/v_tc_0_stub.v
+// Design      : v_tc_0
+// Purpose     : Stub declaration of top-level module interface
+// Device      : xc7a200tsbg484-3
+// --------------------------------------------------------------------------------
+
+// This empty module with port declaration file causes synthesis tools to infer a black box for IP.
+// The synthesis directives are for Synopsys Synplify support to prevent IO buffer insertion.
+// Please paste the declaration into a Verilog source file or add the file as an additional source.
+(* x_core_info = "v_tc,Vivado 2021.2" *)
+module v_tc_0(clk, clken, gen_clken, sof_state, hsync_out, 
+  hblank_out, vsync_out, vblank_out, active_video_out, resetn, fsync_out)
+/* synthesis syn_black_box black_box_pad_pin="clk,clken,gen_clken,sof_state,hsync_out,hblank_out,vsync_out,vblank_out,active_video_out,resetn,fsync_out[0:0]" */;
+  input clk;
+  input clken;
+  input gen_clken;
+  input sof_state;
+  output hsync_out;
+  output hblank_out;
+  output vsync_out;
+  output vblank_out;
+  output active_video_out;
+  input resetn;
+  output [0:0]fsync_out;
+endmodule
