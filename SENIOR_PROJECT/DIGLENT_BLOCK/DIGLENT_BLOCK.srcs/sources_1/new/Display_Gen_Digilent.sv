ceebeb45371ad2846da1841a5419986d80757a26
Evan Leopold <leopole1@lions.tcnj.edu>
2023-10-19 16:22:47 -0400

    Simulation and bitstream for color flood worked.  Simulation for test pattern successful

--- SENIOR_PROJECT/DIGLENT_BLOCK/DIGLENT_BLOCK.srcs/sources_1/new/Display_Gen_Digilent.sv
+++ SENIOR_PROJECT/DIGLENT_BLOCK/DIGLENT_BLOCK.srcs/sources_1/new/Display_Gen_Digilent.sv
@@ -27,7 +27,11 @@ active_video_out,
 
 hsync_out,
 
-vsync_out;
+vsync_out,
+
+h_blank,
+
+v_blank;
 
 
 
@@ -41,11 +45,37 @@ vsync_out;
 /* input  */ logic vid_pVDE  ;
 
 /* input  */ logic vid_pHSync;
+/*input */   logic vid_pVSync;
 
-/* input  */ logic vid_pVSync;
 
-assign vid_pData=24'h00_FF_00;
 
+//Counting and color active_video_out
+
+
+logic[23:0] black, green, yellow, orange, blue, purple, 
+red, white, cyan, magenta, brown, navy_blue, silver, olive, peach;
+
+logic[11:0]disp_x;
+logic[9:0]disp_y;
+
+
+assign black=23'h00_00_00;
+assign green=23'h00_00_FF;
+assign yellow=23'hFF_00_FF;
+assign orange=23'hFF_00_B5;
+assign blue=23'h00_FF_00;
+assign purple=23'h80_80_00;
+assign red=23'hFF_00_00;
+assign white=23'hFF_FF_FF;
+assign cyan=23'h00_FF_FF;
+assign magenta=23'hFF_FF_00;
+assign brown=23'hB5_2A_2A;
+assign navy_blue=23'h00_80_00;
+assign silver=23'hC0_C0_C0;
+assign olive=23'h80_00_80;
+assign peach=23'h00_B9_DA;
+
+/* input  */ 
 
 //reset flip_flop to avoid metastability
 logic res_d1;
@@ -72,9 +102,6 @@ clk_wiz_0 clk_wiz
  // Clock in ports
   .clk_in1(clk)
  );
-
-
-
 
 //Display IP
 rgb2dvi_0 rgb2dvi (
@@ -123,10 +150,62 @@ v_tc_0 vtc(
 
 .hsync_out(vid_pHSync),
 
-.vsync_out(vid_pVSync)
+.vsync_out(vid_pVSync),
+ 
+.hblank_out(h_blank),
 
+.vblank_out(v_blank)
 );
 
+
+//Counter and case statement
+always_ff@(posedge PixelClk) begin
+	if(reset)
+		disp_x<=0;
+	if(~h_blank) begin
+		if(disp_x<1279) 
+			disp_x<=disp_x+1;
+		else
+			disp_x<=0;
+    end	
+end
+
+
+always_ff@(posedge PixelClk) begin
+	if(reset)
+		disp_y<=0;
+	if(~v_blank) begin
+		if(disp_x<1279) begin
+		  if(disp_y<719) 
+			disp_y<=disp_y+1;
+		else
+			disp_y<=0;
+		end
+			
+    end	
+end
+
+always@(disp_x[10:7]) begin
+case(disp_x[10:7])
+    4'b0000: vid_pData=black;
+    4'b0001: vid_pData=blue;
+    4'b0010: vid_pData=orange;
+    4'b0011: vid_pData=green;
+    4'b0100: vid_pData=yellow;
+    4'b0101: vid_pData=cyan;
+    4'b0110: vid_pData=red;
+    4'b0111: vid_pData=silver;
+    4'b1000: vid_pData=purple;
+    4'b1001: vid_pData=magenta;
+    4'b1010: vid_pData=peach;
+    4'b1011: vid_pData=olive;
+    4'b1100: vid_pData=white;
+    4'b1101: vid_pData=navy_blue;
+    4'b1110: vid_pData=brown;
+    4'b1111: vid_pData=yellow;
+    endcase
+end
+    
 
 
 endmodule
