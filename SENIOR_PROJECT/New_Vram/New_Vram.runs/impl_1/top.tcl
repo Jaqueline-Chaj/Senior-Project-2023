f76924ce7992c20a66eb0ba54318962f90c5e6c6
Alex Di Bianco <dibiana1@lions.tcnj.edu>
2023-12-04 21:10:57 -0500

    adding back new vram files

--- SENIOR_PROJECT/New_Vram/New_Vram.runs/impl_1/top.tcl
+++ SENIOR_PROJECT/New_Vram/New_Vram.runs/impl_1/top.tcl
@@ -122,7 +122,11 @@ start_step init_design
 set ACTIVE_STEP init_design
 set rc [catch {
   create_msg_db init_design.pb
+<<<<<<< HEAD
   set_param chipscope.maxJobs 1
+=======
+  set_param chipscope.maxJobs 5
+>>>>>>> 340f42a284bc9acadd016387255c40bf78147a49
 OPTRACE "create in-memory project" START { }
   create_project -in_memory -part xc7a200tsbg484-1
   set_property design_mode GateLvl [current_fileset]
