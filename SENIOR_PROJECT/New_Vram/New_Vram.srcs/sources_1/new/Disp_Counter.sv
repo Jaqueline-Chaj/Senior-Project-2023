626362a9d97b75609d1e29d378d67310e35b9409
Alex Di Bianco <dibiana1@lions.tcnj.edu>
2023-12-04 21:03:14 -0500

    this reverts "removed new vram files" commit
    This reverts commit 4cb42c008d9db97b3bb53e10759b4d9adaafada8.

---
+++ SENIOR_PROJECT/New_Vram/New_Vram.srcs/sources_1/new/Disp_Counter.sv
@@ -0,0 +1,40 @@
+`timescale 1ns / 1ps
+
+
+module Disp_Counter(
+input logic clk, reset,
+output logic[19:0] RD_addr);
+
+logic[10:0] disp_x;
+logic[9:0] disp_y;
+logic[12:0] prod;
+
+always_ff@(posedge clk) begin
+	if(reset)
+		disp_x<=0;
+	else if(disp_x<1649) 
+			disp_x<=disp_x+1;
+		else
+			disp_x<=0;
+    
+end
+
+
+always_ff@(posedge clk) begin
+	if(reset)
+		disp_y<=0;
+	else if(disp_x==1649) begin
+		  if(disp_y<749) 
+			disp_y<=disp_y+1;
+		else
+			disp_y<=0;
+		end
+			
+
+end
+
+assign prod = disp_y * 5;
+  
+assign RD_addr= {prod, 8'b0} +disp_x;  
+
+endmodule
