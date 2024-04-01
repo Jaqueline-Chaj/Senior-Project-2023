bcd77d3c95315ed35ea4f266caefc36277d88d52
Evan Leopold <leopole1@lions.tcnj.edu>
2024-04-01 14:39:21 -0400

    Line engine

--- SENIOR_PROJECT/LineEngine/LineEngine.srcs/sources_1/new/LineEngine.sv
+++ SENIOR_PROJECT/LineEngine/LineEngine.srcs/sources_1/new/LineEngine.sv
@@ -31,12 +31,14 @@ output logic[7:0] line_wr_data
 
     );
     
-assign line_wr_data[7:5]=foreground_color[23:21];
-assign line_wr_data[4:3]=foreground_color[15:14];
-assign line_wr_data[2:0]=foreground_color[7:5];
+assign line_wr_data[7:5]=color[23:21];
+assign line_wr_data[4:3]=color[15:14];
+assign line_wr_data[2:0]=color[7:5];
 logic[10:0] marx;
 logic[9:0] mary;
-
+logic[10:0] x_acc;
+logic[10:0] y_acc;
+logic[10:0] y_acc_neg;
 logic line_state;
 always_ff@(posedge clk) begin  //Sets the conditions for the state machine to progress
 
@@ -54,7 +56,7 @@ logic[10:0] xdiff, ydiff;
 assign xdiff=x2-x1;
 assign ydiff=y2-y1;
 
-logic ydiffabs;
+logic[10:0] ydiffabs;
 
 always@(ydiff) begin
 if(ydiff[10]==1'b1) //Absolute value
@@ -67,40 +69,67 @@ logic[1:0] linecase;
  
 always@(posedge clk) begin   //Detirmining case
    if(xdiff>ydiffabs)begin
-        if(ydiff>0)begin
+        if(ydiff[10] ==0 )begin
             linecase<=2'b00; end
         else begin
             linecase<=2'b01;end
     end
     else begin 
-       if(ydiff>0)begin
+       if(ydiff[10]==0)begin
             linecase<=2'b10; end
         else begin
             linecase<=2'b11;end
    end
 end
-
+logic[11:0] x_compare, y_compare, x_neg_compare, y_neg_compare;
 
 always@(posedge clk) begin
 if(reset || line_state==0) begin
         marx<=x1;
         mary<=y1;
         y_acc<=xdiff;
-        y_acc_neg<=-xdiff;
-        x_acc<=x_diff;
+        x_acc<=ydiffabs;
+       
         end
         
 else if(line_state==1) begin
     case(linecase)
-    00: begin
-        marx<=marx+1;
+    2'b00: begin  //big x, positive y
+        if(marx<x2) begin
+            marx<=marx+1; end
         y_acc<=y_acc+2*ydiff;
         if(y_acc>=2*xdiff) begin
             mary<=mary+1;
-            y_acc<=(y_acc-2*xidff);end
+            y_acc<=(y_acc-2*xdiff);end
         end
-   //Add 01, 10, 11 etc....
+   2'b01: begin //big x, negative y(We 
+        if(marx<x2) begin
+            marx<=marx+1; end;
+        y_acc<=y_acc+2*ydiffabs;
+        if(y_acc>=2*xdiff) begin
+            mary<=mary-1;
+            y_acc<=(y_acc-2*xdiff);end
+        end
+   2'b10: begin  //Small x, positive y
+        if(mary  <  y2) mary<=mary+1;
+        x_acc<=x_acc+2*xdiff;
+        if(x_acc>=2*ydiff)begin
+            marx<=marx+1;
+           x_acc<=x_acc-2*ydiff; end
+        end
+   2'b11: begin  //Small x, negative y
+        if(mary  >  y2) mary<=mary-1;
+        x_acc<=x_acc+2*xdiff;
+        if(x_acc>=2*ydiffabs)begin
+            marx<=marx+1;
+           x_acc<=x_acc-2*ydiffabs; end
+        end
    endcase
+    end
 end
-end
+logic[19:0] prod;
+assign prod=mary * 5;
+assign waddr= {prod, 8'b0} +marx;
+
+assign line_wr_en=line_state;
 endmodule
